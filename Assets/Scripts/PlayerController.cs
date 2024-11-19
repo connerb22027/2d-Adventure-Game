0d6146170a1df2f2433262dcb16deb3ca6f895d4
connerb22027 <Cbourne27@AOITHS.org>
2024-11-19 12:50:53 -0500

    Object Collsions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,15 @@ public class PlayerController : MonoBehaviour
 {  
        
    public InputAction moveAction;
+
+       Rigidbody2D  rigidbody2d;
+       Vector2 move;
    
     // Start is called before the first frame update
     void Start()
     {
        moveAction.Enable();
-       
+       rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -24,4 +27,10 @@ public class PlayerController : MonoBehaviour
         Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
          transform.position = position;
     }
+    void FixedUpdate()
+    { 
+      Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+      rigidbody2d.MovePosition(position);
+    }
+
 }
